8a3e2474f5f793abbf1404840fff4c870ec0f1ec
Sedatyf <gaxel@hotmail.fr>
2021-03-24 15:53:16 +0100

    Create tools.py file which contains main functions

---
+++ src/app/tools.py
@@ -0,0 +1,22 @@
+import json
+import os
+import random
+
+current_path = os.path.dirname(os.path.realpath(__file__))
+sources_path = os.path.join(current_path, "sources.json")
+
+def pick_text():
+    with open(sources_path, "r") as f:
+        json_text = json.load(f)
+
+    text_list = []
+    
+    for _, value in json_text.items():
+        text_list.append(value)
+    
+    random_text = random.choice(text_list)
+    return random_text
+
+
+if __name__ == "__main__":
+    pick_text()
