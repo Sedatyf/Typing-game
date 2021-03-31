b8be1b88d076c673e7aaddf616fe715d2ee5cfb3
Sedatyf <gaxel@hotmail.fr>
2021-03-31 17:33:56 +0200

    Add centered window at launch

--- src/main.py
+++ src/main.py
@@ -1,7 +1,14 @@
-from PySide2 import QtWidgets
+from PySide2 import QtWidgets, QtGui
 import app.gui as gui
 
 app = QtWidgets.QApplication([])
 window = gui.MainWindow()
+
+screen = app.primaryScreen()
+screen_size = screen.size()
+x = (screen_size.width() - window.width()) / 2
+y = (screen_size.height() - window.height()) / 2
+window.move(x, y)
+
 window.show()
 app.exec_()
